7c9d31bddbb548cddcbf45262c296e1a94bf0e05
“Mohsin <syedmohsin8384@gmail.com>
2023-10-03 19:30:06 +0500

    Working on testimonials part

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,14 +1,23 @@
+'use client'
+
 import React from 'react'
+import Image from 'next/image'
+import Button from '@/shared/Button'
+import Video from '@/assets/images/video.png'
 import styles from './hero.module.css'
 
 const Hero = () => {
   return (
-    <div className={styles.mainLayout}>
-      <h1 className={styles.h1}>{`Best AI for UI Ideation`}</h1>
-      <p
-        className={styles.p}
-      >{`Irure dolore exercitation aliquip deserunt aute adipisicing ut. Enim exercitation anim est et consequat pr`}</p>
-    </div>
+    <>
+      <div className={styles.mainLayout}>
+        <h1 className={styles.h1}>{`Best AI for UI Ideation`}</h1>
+        <p
+          className={styles.p}
+        >{`Irure dolore exercitation aliquip deserunt aute adipisicing ut. Enim exercitation anim est et consequat pr`}</p>
+        <Button />
+      </div>
+      <Image className={styles.video} src={Video} />
+    </>
   )
 }
 
---
+++ src/components/Testimonials/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import Card from './Card'
+
+const Testimonials = () => {
+  return <Card />
+}
+
+export default Testimonials
